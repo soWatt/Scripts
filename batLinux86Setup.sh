24a3deb11b9ec26283f6fcb9a8820d0f5983a1b5
sowatt <sowatt@localhost.localdomain>
2023-03-10 10:46:09 -0700

    remove aliases, change to musl binaries

--- batLinux86Setup.sh
+++ batLinux86Setup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 cd ~
-wget "https://github.com/sharkdp/bat/releases/download/v0.22.1/bat-v0.22.1-x86_64-unknown-linux-gnu.tar.gz" 
-tar -xf bat-v0.22.1-x86_64-unknown-linux-gnu.tar.gz
+wget "https://github.com/sharkdp/bat/releases/download/v0.22.1/bat-v0.22.1-x86_64-unknown-linux-musl.tar.gz" 
+tar -xf bat-v0.22.1-x86_64-unknown-linux-musl.tar.gz
 
-CONF=$(~/bat-v0.22.1-x86_64-unknown-linux-gnu/bat --config-dir)
+CONF=$(~/bat-v0.22.1-x86_64-unknown-linux-musl/bat --config-dir)
 mkdir $CONF
-curl https://raw.githubusercontent.com/soWatt/bat-custom-syntaxes/main/syntaxes/PSV.sublime-syntax -o $CONF/PSV.sublime-syntax
+curl https://raw.githubusercontent.com/soWatt/bat-custom-syntaxes/main/syntaxes/PSV.sublime-syntax -o $CONF/syntaxes/PSV.sublime-syntax
 
-~/bat-v0.22.1-x86_64-unknown-linux-gnu/bat cache --build 
-
-echo "alias bat=~/bat-v0.22.1-x86_64-unknown-linux-gnu/bat -l psv" >> ~/.bashrc 
-echo "alias cbat=~/bat-v0.22.1-x86_64-unknown-linux-gnu/bat" >> ~/.bashrc 
+~/bat-v0.22.1-x86_64-unknown-linux-musl/bat cache --build 
 
