190c281216dbcdae3601d8548c249091fea20409
sowatt <sowatt@localhost.localdomain>
2023-03-11 10:04:01 -0700

    add install script via musl binaries for lsd

---
+++ lsdLinux86Setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ~
+
+wget "https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd-0.23.1-x86_64-unknown-linux-musl.tar.gz"
+
+tar -xf lsd-0.23.1-x86_64-unknown-linux-musl.tar.gz
+
