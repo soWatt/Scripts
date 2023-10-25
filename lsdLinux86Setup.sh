f36f60418c20d41864637d568fdff7d1bdf12557
sowatt <swattyk@gmail.com>
2023-10-25 12:46:22 -0500

    updating versions for lsd and bat

--- lsdLinux86Setup.sh
+++ lsdLinux86Setup.sh
@@ -2,7 +2,8 @@
 
 cd ~
 
-wget "https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd-0.23.1-x86_64-unknown-linux-musl.tar.gz"
+wget "https://github.com/lsd-rs/lsd/releases/download/v1.0.0/lsd-v1.0.0-x86_64-unknown-linux-musl.tar.gz"
 
-tar -xf lsd-0.23.1-x86_64-unknown-linux-musl.tar.gz
+tar -xf lsd-v1.0.0-x86_64-unknown-linux-musl.tar.gz
+ 
 
